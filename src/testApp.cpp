d1695e0ac04ddf903b41d15bd5d60a717ccd335a
Martial <martialou@gmail.com>
2013-10-15 16:57:42 +0200

    First commit

---
+++ src/testApp.cpp
@@ -0,0 +1,30 @@
+#include "testApp.h"
+
+void testApp::setup() {
+	
+    ofBackground(0,0,0);
+    ofSetFrameRate(32);
+    
+    
+    
+}
+
+
+
+void testApp::update() {
+
+}
+
+void testApp::draw() {
+ 
+    
+}
+
+
+void testApp::keyPressed(int key) {
+    }
+
+
+void testApp::exit(){
+}
+
